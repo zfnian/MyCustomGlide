c4b4b5cf9fcf46c0bab1b03215cd5adc08e12c56
zhaofengnian <fengnian.zhao@beantechs.com>
2020-04-25 12:52:41 +0800

    add source

---
+++ CustomGlide/app/src/main/java/com/example/customglide/resource/Key.java
@@ -0,0 +1,24 @@
+package com.example.customglide.resource;
+
+import com.example.customglide.Tool;
+
+/**
+ * key  ----->  Bitmap封装   === Value
+ */
+public class Key {
+
+    private String key; // 合格：唯一 加密的  ac037ea49e34257dc5577d1796bb137dbaddc0e42a9dff051beee8ea457a4668
+
+
+    public Key(String path) {
+        this.key = Tool.getSHA256StrJava(path);
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
